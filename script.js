9d1dcf39c17fa222338bf70d639049642d415a16
acmong2212 <depzai02x@gmail.com>
2021-10-26 23:46:41 +0700

    Di chuyen pikachu

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
-/**
- * Created by nhatnk on 4/26/17.
- */
-
-function Hero(image, top, left, size){
+function Hero(image, top, left, size, speed) {
   this.image = image;
   this.top = top;
   this.left = left;
   this.size = size;
+  this.speed = speed;
 
-  this.getHeroElement = function(){
-    return '<img width="'+ this.size + '"' +
-      ' height="'+ this.size + '"' +
-      ' src="' + this.image +'"' +
-      ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+
+  this.getHeroElement = function () {
+    return '<img alt="khonghienthi" width="' + this.size + '"' +
+        ' height="' + this.size + '"' +
+        ' src="' + this.image + '"' +
+        ' style="top: ' + this.top + 'px; left:' + this.left + 'px;position:absolute;" />';
   }
 
-  this.moveRight = function(){
-    this.left += 20;
+  this.moveRight = function () {
+    this.left += this.speed;
     console.log('ok: ' + this.left);
   }
-
+  this.moveBottom = function () {
+    this.top += this.speed;
+    console.log('ok: ' + this.top);
+  }
+  this.moveLeft = function () {
+    this.left -= this.speed;
+    console.log('ok: ' + this.left);
+  }
+  this.moveTop = function () {
+    this.top -= this.speed;
+    console.log('ok: ' + this.top);
+  }
 }
 
-var hero = new Hero('pikachu.png', 20, 30, 200);
+let speed = +prompt("Nhập vào tốc độ: ")
+let hero = new Hero('pikachu.png', 0, 0, 200, speed);
 
-function start(){
-  if(hero.left < window.innerWidth - hero.size){
+function start() {
+  if (hero.top === 0 && hero.left < window.innerWidth - hero.size) {
     hero.moveRight();
+  } else if (hero.top < window.innerHeight - hero.size && hero.left >= window.innerWidth - hero.size) {
+    hero.moveBottom();
+  } else if (hero.left > 0) {
+    hero.moveLeft()
+  } else if (hero.top > 0) {
+    hero.moveTop()
   }
   document.getElementById('game').innerHTML = hero.getHeroElement();
-  setTimeout(start, 500)
+  setTimeout(start, 20)
 }
 
 start();
